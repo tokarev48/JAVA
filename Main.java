141b0b81c2fd2c490423f8e2d4c2cc78032f68ca
tokarev48 <113343606+tokarev48@users.noreply.github.com>
2023-05-01 22:03:13 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Matrix m = new Matrix(5);
+        System.out.println(m);
+        System.out.println("Sum"+" = "+m.sum());
+        System.out.println("mult"+" = "+m.mult());
+        System.out.println("diff"+" = "+m.diff());
+    }
+}
